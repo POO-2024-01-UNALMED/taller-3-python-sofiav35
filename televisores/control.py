7f5a1bbf4f7305d13835a140301e21d4b8ae9031
sofiav35 <159485936+sofiav35@users.noreply.github.com>
2024-02-25 13:35:19 -0500

    .

--- televisores/control.py
+++ televisores/control.py
@@ -6,27 +6,27 @@ class Control:
     def turnOn(self):
         self._tv.turnOn()
     def turnOff(self):
-        self._tv.turnOff
+        self._tv.turnOff()
 
 #Canales control
     def canalUp(self):
-        self._tv.canalUp
+        self._tv.canalUp()
     def canalDown(self):
-        self._tv.canalDown
+        self._tv.canalDown()
 
 #volumen control
     def volumenUp(self):
         self._tv.volumenUp
     def volumenDown(self):
-        self._tv.volumenDown
+        self._tv.volumenDown()
 
 #set Canal
     def setCanal(self):
-        self._tv.setCanal
+        self._tv.setCanal()
 
 #set Volumen
     def setVolumen(self):
-        self._tv.setVolumen  
+        self._tv.setVolumen()  
     
     def enlazar(self, tv):
         self._tv=tv
